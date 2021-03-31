8bb8495c919d87d1b29b5db241176f642aa895ad
Tony Sorensen <tony48853@gmail.com>
2021-03-30 20:22:55 -0700

    Provider, store, middleware set up in index.js

--- app/src/index.js
+++ app/src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { createStore,applyMiddleware } from "redux";
+import  thunk from "redux-thunk";
+import reducer from "/reducers";
+import { createLogger } from "redux-logger";
+
+const logger = createLogger();
+const initialState={};
+const store = createStore(reducer, initialState, applyMiddleware(thunk,logger))
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <Provider store={store}>
+  <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
